6da6ee2bbb83e098e0520cafc572f897c2acc4bd
NEIL <king0x000@gmail.com>
2021-09-14 19:18:36 +0800

    tmp hp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from './app.service';
 import { SwUpdate } from '@angular/service-worker';
+import { range } from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -14,11 +15,56 @@ export class AppComponent implements OnInit {
         private swUpdate: SwUpdate
     ) { }
 
-    public ngOnInit(): void {
+    ngOnInit(): void {
         this.swUpdate.available.subscribe(() => {
             this.service.hasNewUpdate = true;
         });
     }
 
+    temphpcalc(): void {
+        // chara
+        let charaLv = 0;
+        let charaGradeup = 0;
+        let charaMinHP = 0;
+        let charaMaxHP = 0;
 
+        // gears
+        let shotGears = [];
+        let closeGears = [];
+        let topGears = [
+            {
+                minLv: 0,
+                maxLv: 0,
+                minHP: 0,
+                maxHP: 0,
+                minQuality: 0,
+                maxQuality: 0,
+            }
+        ]
+        let bottomGears = [
+            {
+                minLv: 0,
+                maxLv: 0,
+                minHP: 0,
+                maxHP: 0,
+            }
+        ]
+        let shotcustomKits = [
+            0.3, 0.5, 0.7
+        ];
+
+        const calcSet = {
+            charaBaseHP: 0,
+            topGearBaseHP: 0,
+            topGearQuality: 0,
+            topGearCustomKit: 0,
+            bottomGearBaseHP: 0,
+            bottomGearCustomKit: 0,
+        }
+    }
+    *combinations(arrOfArr) {
+        let [head, ...tail] = arrOfArr
+        let remainder = tail.length ? this.combinations(tail) : [[]];
+        for (let r of remainder) for (let h of head) yield [h, ...r];
+    }
 }
