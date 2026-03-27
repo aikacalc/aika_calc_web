7bcd4229758070a946cb94fd5cc5042d4d70bebb
aikacalc <king0x000+aikacalc@gmail.com>
2026-03-27 21:01:48 +0800

    test

--- src/app/aika-gacha-calc/aika-gacha-calc.component.spec.ts
+++ src/app/aika-gacha-calc/aika-gacha-calc.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { AikaGachaCalcComponent } from './aika-gacha-calc.component';
 
@@ -8,7 +10,8 @@ describe('AikaGachaCalcComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AikaGachaCalcComponent]
+      imports: [AikaGachaCalcComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()]
     })
     .compileComponents();
 
